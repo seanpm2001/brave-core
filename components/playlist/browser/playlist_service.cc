3115db8ba3a5e2ac6902c45f65c92fc00010da2e
deeppandya <deeppandya91@gmail.com>
2023-08-18 13:08:57 -0400

    Update soilution for downloading HLS content

--- components/playlist/browser/playlist_service.cc
+++ components/playlist/browser/playlist_service.cc
@@ -1006,6 +1006,10 @@ void PlaylistService::AddObserverForStreaming(
   streaming_observers_.Add(std::move(observer));
 }
 
+void PlaylistService::ClearObserverForStreaming() {
+  streaming_observers_.Clear();
+}
+
 void PlaylistService::OnMediaUpdatedFromContents(
     content::WebContents* contents) {
   if (download_request_manager_->background_contents() != contents) {
@@ -1173,8 +1177,8 @@ void PlaylistService::OnResponseStarted(const std::string& url,
 void PlaylistService::OnDataReceived(
     data_decoder::DataDecoder::ValueOrError result) {
   if (!result.has_value()) {
-    LOG(ERROR) << "data_source : "
-               << "PlaylistService::OnDataReceived : empty";
+    // LOG(ERROR) << "data_source : "
+    //            << "PlaylistService::OnDataReceived : empty";
     return;
   }
 
@@ -1194,16 +1198,16 @@ void PlaylistService::OnDataReceived(
 void PlaylistService::OnDataComplete(
     api_request_helper::APIRequestResult result) {
   if (result.Is2XXResponseCode()) {
-    if (!result.headers().empty()) {
-      for (auto entry : result.headers()) {
-        LOG(ERROR) << "data_source : "
-                   << "PlaylistService::OnDataComplete : "
-                   << "entry.first : \n"
-                   << entry.first
-                   << "\nOnAPIStreamDataComplete entry.second : \n"
-                   << entry.second;
-      }
-    }
+    // if (!result.headers().empty()) {
+    //   for (auto entry : result.headers()) {
+    //     LOG(ERROR) << "data_source : "
+    //                << "PlaylistService::OnDataComplete : "
+    //                << "entry.first : \n"
+    //                << entry.first
+    //                << "\nOnAPIStreamDataComplete entry.second : \n"
+    //                << entry.second;
+    //   }
+    // }
 
     for (auto& observer : streaming_observers_) {
       observer->OnDataCompleted();
