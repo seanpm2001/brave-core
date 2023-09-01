c327526f55967439be6852a78d8d80e7e39cf781
Claudio DeSouza <cdesouza@brave.com>
2023-09-01 20:20:45 +0100

    `application_context` target moved to `model` subfolder
    This is merely a change in path for these inclusions/target.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/7f06d09e7e5f8b0e1fc6ff13a322c538876fa5c0
    
    commit 7f06d09e7e5f8b0e1fc6ff13a322c538876fa5c0
    Author: Gauthier Ambard <gambard@chromium.org>
    Date:   Tue Aug 22 16:30:48 2023 +0000
    
        [iOS] Move ios/c/b/application_context to model subfolder
    
        To simplify the folder hierarchy on iOS and have a clearer layer
        separation, all the files in ios/c/b/<foo>/ will be moved to
        ios/c/b/<foo>/model/ to showcase that they are model files.
    
        Bug: 1319852

--- chromium_src/ios/chrome/browser/application_context/application_context_impl.mm
+++ chromium_src/ios/chrome/browser/application_context/application_context_impl.mm
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "ios/chrome/browser/application_context/application_context_impl.h"
+#include "ios/chrome/browser/application_context/model/application_context_impl.h"
 
 #include <vector>
 
