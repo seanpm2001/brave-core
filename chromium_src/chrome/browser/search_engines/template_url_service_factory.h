8dfdbc85c6acf409a28d63e76aeb3fba7927ff17
Claudio DeSouza <cdesouza@brave.com>
2023-09-01 12:00:55 +0100

    Reanchoring `TemplateURLServiceFactory` override
    The substitution for `GetBrowserContextToUse` got dropped due to a
    recent change, and this change reanchors this override to another
    method.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/0e8dbbf992ff0f30184628473ea6775a70eb0283
    
    commit 0e8dbbf992ff0f30184628473ea6775a70eb0283
    Author: Arthur Milchior <arthurmilchior@chromium.org>
    Date:   Tue Aug 22 16:37:48 2023 +0000
    
        Implement the non-deprecated builder.
    
        Any class inheriting `BrowserContextKeyedServiceFactory` should
        implement either `BuildServiceInstanceForBrowserContext` or
        `BuildServiceInstanceFor`. The second one was deprecated,
        this CL replaces it with the first one.
    
        Bug: 1396138

--- chromium_src/chrome/browser/search_engines/template_url_service_factory.h
+++ chromium_src/chrome/browser/search_engines/template_url_service_factory.h
@@ -10,14 +10,14 @@
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
 #include "content/public/browser/browser_context.h"
 
-#define BuildServiceInstanceFor                         \
-  BuildServiceInstanceFor_unused();                     \
+#define BrowserContextDestroyed                         \
+  BrowserContextDestroyed_unused();                     \
   content::BrowserContext* GetBrowserContextToUse(      \
       content::BrowserContext* context) const override; \
-  KeyedService* BuildServiceInstanceFor
+  void BrowserContextDestroyed
 
 #include "src/chrome/browser/search_engines/template_url_service_factory.h"  // IWYU pragma: export
 
-#undef BuildServiceInstanceFor
+#undef BrowserContextDestroyed
 
 #endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_SEARCH_ENGINES_TEMPLATE_URL_SERVICE_FACTORY_H_
